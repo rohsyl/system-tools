b155617236791c58b8e477d0e699963575c2d4ea
Gaétan <huser.gaetan@hotmail.com>
2019-04-17 13:13:19 +0200

    newvhost : adding optional alternate path to data

--- linux/services/apache/newvhost.sh
+++ linux/services/apache/newvhost.sh
@@ -13,31 +13,44 @@ fi
 
 if [ $1 = "--help" ]
 then
-	echo "$0 [ SITE_NAME ]"
+	echo "$0 [ SITE_NAME ] [ PATH_TO_DATA (optional) ]"
+	echo ""
 	echo "Create new virtual host in apache2 named SITE_NAME"
+	echo "PATH_TO_DATA is the absolute path to the public folder of your site if not located in /var/www"
+	
 	exit 0
 fi
 
 www=/var/www/sites
 sn=$1
 
+if [ $2 ] 
+then
+	path_to_dir=$2
+else
+	path_to_dir="$www/$sn/public_html"
+fi
+
 # Create the file with VirtualHost configuration in /etc/apache2/site-available/
 echo Create apache config file under /etc/apache2/sites-available/$sn.conf [OK]
 echo "<VirtualHost *:80>
     ServerAdmin webmaster@$sn
 	ServerName $sn
-	DocumentRoot $www/$sn/public_html
+	
+	DocumentRoot $path_to_dir
+	
 	ErrorLog $www/$sn/logs/error.log 
 	CustomLog $www/$sn/logs/access.log combined
 	
-	<Directory $www/$sn/public_html>
+	<Directory $path_to_dir>		
 		Options Indexes FollowSymLinks MultiViews
 		AllowOverride All
 		Order allow,deny
 		allow from all
 		Require all granted
 	</Directory>
-</VirtualHost>" > /etc/apache2/sites-available/$sn.conf
+</VirtualHost>
+" > /etc/apache2/sites-available/$sn.conf
 
 #Create diretory
 echo Create directories $www/$sn [OK]
